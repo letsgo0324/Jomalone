6af3caf73f1e4891b3a668a0ee83798ca04c0657
letsgo0324 <letsgo0324@naver.com>
2022-03-23 01:32:41 +0900

    메인 비주얼 anime 적용

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//햄버거 메뉴
 const btnCall = document.querySelector(".btnCall"); 
 const menuMo = document.querySelector(".menuMo"); 
 
@@ -7,3 +8,28 @@ btnCall.onclick = function(e){
     btnCall.classList.toggle("on"); 
     menuMo.classList.toggle("on"); 
 }
+
+//비주얼 슬라이더
+const visual = document.querySelector("#visual");
+const visualSliders = visual.querySelector(".visual_sliders");
+const btnCircle = document.querySelectorAll(".btnCircle li");
+
+btnCircle.forEach((el,index)=>{
+    el.addEventListener("click", e=>{
+        e.preventDefault();
+
+        let isOn = e.currentTarget.classList.contains("on");
+        if(isOn) return;
+
+        new Anime(visualSliders, {
+            prop: "margin-left", 
+            value: -100 * index + "%",
+            duration: 1000
+        }); 
+
+        for(let el of btnCircle){
+            el.classList.remove("on");
+        }
+        el.classList.add("on");
+    })    
+})
