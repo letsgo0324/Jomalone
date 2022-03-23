3d93810b2cb842073961e639d8d18d8f62c40605
letsgo0324 <letsgo0324@naver.com>
2022-03-24 01:44:14 +0900

    메인 탭메뉴 추가

--- js/main_slider.js
+++ js/main_slider.js
@@ -22,3 +22,38 @@ btnCircle.forEach((el,index)=>{
         el.classList.add("on");
     })    
 })
+
+//탭 메뉴
+const tab = document.querySelector("#tab");
+const dts = tab.querySelectorAll("dt");
+const dds = tab.querySelectorAll("dd");
+const dtsFocus = tab.querySelectorAll("dt>a");
+
+dtsFocus.forEach((el,index)=>{
+
+    el.addEventListener("focusin", ()=>{
+        
+        activation(index, dts);
+        activation(index, dds);
+    })
+})
+
+dts.forEach((dt,index)=>{
+
+    dt.addEventListener("click", e=>{
+        e.preventDefault();
+
+        let isOn = e.currentTarget.classList.contains("on");
+        if(isOn) return;
+
+        activation(index, dts);
+        activation(index, dds);
+    })
+})
+
+function activation(index, items){
+    for(let el of items){
+        el.classList.remove("on");
+    }
+    items[index].classList.add("on");    
+}
