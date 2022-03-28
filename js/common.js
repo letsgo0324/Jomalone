69158d449ce8c60b14f5b6d9ceae74c605280a3c
letsgo0324 <letsgo0324@naver.com>
2022-03-28 23:38:02 +0900

    유투브 추가,헤더명 변경

--- js/common.js
+++ js/common.js
@@ -2,9 +2,16 @@
 const btnCall = document.querySelector(".btnCall"); 
 const menu = document.querySelector(".menu"); 
 
+init();
+
 btnCall.onclick = function(e){
     e.preventDefault(); 
 
     btnCall.classList.toggle("on"); 
     menu.classList.toggle("on"); 
+}
+
+function init(){
+    btnCall.classList.add("on");
+    menu.classList.add("on");
 }
